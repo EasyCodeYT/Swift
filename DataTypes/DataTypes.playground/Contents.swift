1a140acb2ca814212f5858100fd9a39c1d9e7c52
SADDAM HUSAIN <saddam.husain1@timesinternet.in>
2022-08-08 20:11:42 +0530

    data types

---
+++ DataTypes/DataTypes.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var charValue: Character = "Q"
+
+var textValue : String = "Sasdhgfsajhsfg"
+
+var intValue: Int = 300
+
+var floatValue: Float = 3.99
+
+var doubleValue: Double = 67.99
+
+var boolValue: Bool = true
