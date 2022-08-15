f301f43e63701ed24089d64bb3acd8c788fd7723
SADDAM HUSAIN <saddam.husain1@timesinternet.in>
2022-08-15 11:39:27 +0530

    control flow

---
+++ If...else/If...else...Statement.playground/Contents.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+// syntax of if statement
+
+//if (condition) {
+//  // body of if statement
+//}
+
+let number = 10
+
+// check if number is greater than 0
+if (number > 0) {
+  print("Number is positive.")
+}
+
+
+// syntax of if...else statement
+
+//if (condition) {
+//  // block of code if condition is true
+//}
+//else {
+//  // block of code if condition is false
+//}
+
+let number2 = -20
+
+if (number2 > 0) {
+    print("Number2 is positive.")
+}
+else {
+    print("Number2 is negative.")
+}
+
+// syntax of if...else if...else Statement
+
+//if (condition1) {
+//    // code block 1
+//}
+//else if (condition2){
+//    // code block 2
+//}
+//else {
+//    // code block 3
+//}
+//
+//print("any message")
+
+// check whether a number is positive, negative, or 0.
+
+let number3 = 0
+
+if (number3 > 0) {
+    print("Number3 is positive.")
+}else if (number3 < 0) {
+    print("Number3 is negative")
+}else {
+    print("Number3 is 0.")
+}
+
+print("This statement is always executed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
