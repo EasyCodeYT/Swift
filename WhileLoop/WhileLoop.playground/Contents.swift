5aa0a12945fb5d3549accaa6b88636f36af31c9e
SADDAM HUSAIN <saddam.husain1@timesinternet.in>
2022-10-02 14:10:15 +0530

    while loop

---
+++ WhileLoop/WhileLoop.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+//while (condition){
+//  // body of loop
+//}
+
+
+// program to display numbers from 1 to 5
+// initialize the variable
+var i = 1, n = 5
+
+// while loop from i = 1 to 5
+while (i <= n) {
+  print(i)
+   i = i + 1
+}
+
+
+// program to display numbers
+
+var j = 1, k = 5
+
+// repeat...while loop from 1 to 5
+repeat {
+  
+  print(j)
+
+  j = j + 1
+
+} while (j <= k)
